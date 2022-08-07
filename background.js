9dd4e15088a1aa43610759b0458205ddb7d0153d
matteo <matteochirco@gmail.com>
2022-08-06 21:40:29 -0700

    Please enter the commit message for your changes. Lines starting update

--- background.js
+++ background.js
@@ -13,7 +13,18 @@ chrome.runtime.onMessage.addListener(function(request) {
 });
 
 //listener for context menu
-chrome.contextMenus.onClicked.addListener(function generateOnclick(info){
-    //console.log("https://www.amazon.com/gc/redeem?code="+ info.selectionText +"&skipReveal=true");
-    chrome.tabs.create({url : "https://www.amazon.com/gc/redeem?code="+ info.selectionText +"&skipReveal=true"});
-} )
+chrome.contextMenus.onClicked.addListener( function generateOnclick(info){
+    // //console.log("https://www.amazon.com/gc/redeem?code="+ info.selectionText +"&skipReveal=true");
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    //       console.log(response.farewell);
+    //     });
+    //   });
+    chrome.tabs.create({url: 'http://google.de'}, function (tab) {  //inject content script
+    console.info("the tab is: " + tab.id)
+        chrome.tabs.sendMessage(tab.id, {greeting: "hello"});  //send message to content script
+    
+});
+
+
+})
--- content.js
+++ content.js
@@ -1,4 +1,7 @@
+
+
 document.addEventListener("mousedown", function(event){
+    //alert("hello world");
     var selection = window.getSelection().toString();
     if(selection.match(/^[a-zA-Z0-9]{4}-[a-zA-Z0-9]{6}-[a-zA-Z0-9]{4}$/)) {
         chrome.runtime.sendMessage({cmd: "create_menu"});
@@ -6,3 +9,18 @@ document.addEventListener("mousedown", function(event){
         chrome.runtime.sendMessage({cmd: "delete_menu"});
     }
 }, true); 
+
+document.addEventListener("DOMContentLoaded", (event) => {
+//alert("DOMContentLoaded")
+})
+
+ chrome.runtime.onMessage.addListener(function(request,sender, sendResponse){
+    alert("received")
+    console.log("received")
+    document.getElementById('sb_form_q').value="hello world";
+    //     console.log(sender.tab ?
+//         "from a content script:" + sender.tab.url :
+//         "from the extension");
+//    // chrome.tabs.create({url : "https://www.google.com"});
+//     alert("dd")
+ })
