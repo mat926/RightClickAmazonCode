72f81fc915d51f3748df50ca751cb99dcbaa7371
Matt Chirco <matteochirco@gmail.com>
2024-02-14 18:51:42 -0700

    Modify regex searching

--- background.js
+++ background.js
@@ -16,10 +16,16 @@ chrome.runtime.onMessage.addListener(function(request) {
 chrome.contextMenus.onClicked.addListener((info) => {
     chrome.tabs.create({
         url: "https://www.amazon.com/gc/redeem"}, function(tab) {
-        chrome.scripting.executeScript({target: {tabId: tab.id}, args: [info.selectionText],
+        chrome.scripting.executeScript({target: {tabId: tab.id}, args: [extractClaimCode(info.selectionText)],
             func: (code)=> {
                 document.getElementById("gc-redemption-input").value=code
             }
         });
       });
 })
+
+function extractClaimCode(text) {
+    const pattern = /[a-zA-Z0-9]{4}-[a-zA-Z0-9]{6}-[a-zA-Z0-9]{4}/
+    let match = text.match(pattern);
+    return match !== null ? match[0] : null;
+}
--- content.js
+++ content.js
@@ -1,9 +1,15 @@
 //only make the menu appear when a claim code is selected
 document.addEventListener("mousedown", function(event){
     var selection = window.getSelection().toString();
-    if(selection.match(/^\s*[a-zA-Z0-9]{4}-[a-zA-Z0-9]{6}-[a-zA-Z0-9]{4}\s*$/)) {
+    if(containsValidClaimCode(selection)) {
         chrome.runtime.sendMessage({cmd: "create_menu"});
     } else {
         chrome.runtime.sendMessage({cmd: "delete_menu"});
     }
 }, true); 
+
+function containsValidClaimCode(claimCode) {
+    claimCode = claimCode.trim();
+    const pattern = /[a-zA-Z0-9]{4}-[a-zA-Z0-9]{6}-[a-zA-Z0-9]{4}/
+    return claimCode.match(pattern) !== null;
+}
